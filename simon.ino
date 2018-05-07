ab3239bc599b70e2b295d86889aad5627b0fece0
amaggiacomo <39041368+amaggiacomo@users.noreply.github.com>
2018-05-07 16:29:48 -0400

    Update simon.ino

--- simon.ino
+++ simon.ino
@@ -1,3 +1,5 @@
+//Below is the initilization of any variables associated with pins. This is done so the pins in question are easier to identify and debug
+
 const int greenButtonPin = 2;
 const int greenLED =  11;
 const int blueButtonPin = 3;
@@ -5,18 +7,18 @@ const int blueLED =  12;
 const int redButtonPin = 4;
 const int redLED =  13;
 const int debugLED = 6;
+const int buzzer = 9;
 
+//Below is where the remaining variables are declared and initilized
 int sequence[20];
 int mostRecent = -1;
 int counter = 1;
 bool takeTurn = true;
-
-const int buzzer = 9;
-
 int greenButtonState = 0;
 int blueButtonState = 0;
 int redButtonState = 0;
 
+//Sets all of the proper pins in the correct pinmode
 void setup() {
   pinMode(greenLED, OUTPUT);
   pinMode(greenButtonPin, INPUT);
@@ -24,18 +26,20 @@ void setup() {
   pinMode(blueButtonPin, INPUT);
   pinMode(redLED, OUTPUT);
   pinMode(redButtonPin, INPUT);
-
   pinMode(debugLED, OUTPUT);
-
   pinMode(buzzer, OUTPUT);
 }
 
+//The method "loop". This method is constantly looping. The for loop inside only runs when it is time for the AI to take its "turn"
+//Which involves it lighting up a sequence of random LEDs.
 void loop() {
 
   for (int i = 0; i < counter && takeTurn; i++) {
 
     if (i == 0) {
+      //Calling randomSeed is needed, or else the "random" sequence will always be the same
       randomSeed(analogRead(0));
+      //Sets the next int in the sequence equal to a random number that is either a 0, 1 or 2.
       sequence[counter-1] = random(0, 3);
     }
 
@@ -67,8 +71,9 @@ void loop() {
 
   }
 
+  
+  //While loop only runs when it is not the computer's turn.
   while (takeTurn == false) {
-
 
     for (int i = 0; i < counter; i++) {
       mostRecent = -1;
@@ -83,6 +88,8 @@ void loop() {
     }
     counter = counter + 1;
 
+    //The below commented out code was used for debugging purposes.
+    
 /*
     for(int j = 0;j<counter;j++){
       digitalWrite(debugLED, HIGH);
@@ -99,6 +106,8 @@ void loop() {
 
 void pushable() {
 
+  //Waits for user input, as mostRecent will be -1 forever unless the user pushes a button. When that occurs, not only does it break out of
+  //the loop, but now mostRecent is logged as the button address that the user just pushed.
   while (mostRecent == -1) {
 
     greenButtonState = digitalRead(greenButtonPin);
@@ -144,6 +153,8 @@ void gameover(){
   exit(0);
   
 }
+
+//All of the below methods are for turning LED's on and off. When an LED is turned on, its corresponding buzzer noise is also activated
 
 void greenON() {
   digitalWrite(greenLED, HIGH);
